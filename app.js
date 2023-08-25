84461e08aa468b1b978f0372cf4c0c8a3cd36358
guilhermelima <guilhermelimaruby@gmail.com>
2023-08-25 18:13:26 -0300

    aula_9

--- app.js
+++ app.js
@@ -14,6 +14,16 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
+
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
+
+
     container.appendChild(cartao)
 
 }
